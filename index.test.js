8d1ef4addf38717da8cddf9bfc3827324e826de2
Atte Liimatainen <atte.liimat@gmail.com>
2021-08-17 10:38:17 +0300

    remove unused imports

--- index.test.js
+++ index.test.js
@@ -1,7 +1,6 @@
 import React from "react"
-import Enzyme, { shallow, mount, render } from "enzyme"
+import Enzyme, { shallow, mount } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
-import jest from "jest"
 
 import SplitText from "./index"
 
